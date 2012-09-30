31ed81a3b281dccf21d43e3eb86b502b09340660
laurocaetano <laurocaetano1@gmail.com>
2012-09-29 23:43:09 -0300

    writing a complete word

--- spec/keyboard_spec.rb
+++ spec/keyboard_spec.rb
@@ -3,9 +3,14 @@ require 'keyboard'
 
 describe Keyboard do
   context "Given a message with only a single word" do
-    it "should write a" do
+    it "should write 'a'" do
       keyboard = Keyboard.new
-      keyboard.write('2').should == 'a'
+      keyboard.write('a').should == '2'
+    end
+    
+    it "should write 'text'" do
+      keyboard = Keyboard.new
+      keyboard.write('text').should == '833998'
     end
   end
 end
